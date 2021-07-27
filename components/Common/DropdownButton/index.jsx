3e08b14e4cf85b2b1f74dc694ec247a7e3204594
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2021-07-27 19:47:12 -0300

    dropdown on hover

--- components/Common/DropdownButton/index.jsx
+++ components/Common/DropdownButton/index.jsx
@@ -44,20 +44,44 @@ const DropdownButton = ({
                 fontSize={fontSize}
                 // width={width}
                 fullWidth={fullWidth}
-                onClick={() => {
+                onMouseEnter={() => {
                     const buttonElement = document.getElementById(id)
-                    if (buttonElement) {
-                        // console.log('top: ', top)
-                        // console.log('buttonElement.getBoundingClientRect(): ', buttonElement.getBoundingClientRect())
-        // console.log('left: ', left)
+                        setTop(buttonElement.getBoundingClientRect().top)
+                        setLeft(buttonElement.getBoundingClientRect().left)
+                        setButtonWidth(buttonElement.getBoundingClientRect().width)
+                        setShowDropdown(id)
+                }}
+                onMouseOver={() => {
+                    const buttonElement = document.getElementById(id)
+                    if (buttonElement && !showDropdown) {
+                        setTop(buttonElement.getBoundingClientRect().top)
+                        setLeft(buttonElement.getBoundingClientRect().left)
+                        setButtonWidth(buttonElement.getBoundingClientRect().width)
+                        setShowDropdown(id)
+                    }
+                }}
+                onMouseLeave={() => {
+                    const buttonElement = document.getElementById(id)
 
                         setTop(buttonElement.getBoundingClientRect().top)
                         setLeft(buttonElement.getBoundingClientRect().left)
                         setButtonWidth(buttonElement.getBoundingClientRect().width)
-                        showDropdown === id ? setShowDropdown(false) : setShowDropdown(id)
-                    }
-
+                        setShowDropdown(false)
                 }}
+        //         onClick={() => {
+        //             const buttonElement = document.getElementById(id)
+        //             if (buttonElement) {
+        //                 // console.log('top: ', top)
+        //                 // console.log('buttonElement.getBoundingClientRect(): ', buttonElement.getBoundingClientRect())
+        // // console.log('left: ', left)
+
+        //                 setTop(buttonElement.getBoundingClientRect().top)
+        //                 setLeft(buttonElement.getBoundingClientRect().left)
+        //                 setButtonWidth(buttonElement.getBoundingClientRect().width)
+        //                 showDropdown === id ? setShowDropdown(false) : setShowDropdown(id)
+        //             }
+
+        //         }}
                 margin={margin}
                 padding={padding}
             >
