d0ac958b82d5b747f64b6f058c84cf78c560adc9
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2021-08-03 13:58:30 -0300

    footer update

--- components/Layout/Footer/index.jsx
+++ components/Layout/Footer/index.jsx
@@ -1,10 +1,26 @@
-import { FooterContainer } from "./styles"
+import { FooterCol, FooterContainer, FooterItem } from "./styles"
 
 
 const Footer = () => {
     return (
         <FooterContainer>
-            Footer
+            <FooterCol>
+                Copyright &copy; 2021
+                <br/>
+                <br/>
+                <i>Lumiverso</i>
+            </FooterCol>
+            <FooterCol>
+                <FooterItem>Curso Jardinagem Fácil</FooterItem>
+                <FooterItem>eBook</FooterItem>
+                <FooterItem>Produto</FooterItem>
+            </FooterCol>
+            <FooterCol>
+                <FooterItem>Empresa</FooterItem>
+                <FooterItem>Sobre</FooterItem>
+                <FooterItem>Notícias</FooterItem>
+                <FooterItem>Contato</FooterItem>
+            </FooterCol>
         </FooterContainer>
     )
 }
--- components/Layout/Footer/styles.js
+++ components/Layout/Footer/styles.js
@@ -6,9 +6,35 @@ export const FooterContainer = styled.footer`
     padding: 1rem;
 
     display:flex;
-    align-items: center;
     justify-content: center;
-
+    flex-wrap: wrap;
     border-top: 1px solid ${({ theme }) => theme.colors.text.neutral400};
 
+    background: ${({ theme }) => theme.colors.primary.dark};
+`
+
+export const FooterCol = styled.div`
+    font-size: 17px;
+    font-weight: 600;
+    color: #eee;
+    margin: 80px;
+    margin-top: 30px;
+    margin-bottom: 10px;
+
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+
+    @media(max-width: 885px) {
+        width: 250px;
+    
+    }
+`
+
+export const FooterItem = styled.div`
+    cursor: pointer;
+    margin-bottom: 20px;
+    &:hover {
+        color: ${({ theme }) => theme.colors.primary.purple};
+    }
 `
